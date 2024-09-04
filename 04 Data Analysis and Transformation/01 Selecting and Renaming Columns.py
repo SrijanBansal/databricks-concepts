b1a25242c389f91b1ac02695762efb3dee982638
SrijanBansal <SrijanBansal>
2024-09-04 14:03:05 +0000

    addingFiles

--- 04 Data Analysis and Transformation/01 Selecting and Renaming Columns.py
+++ 04 Data Analysis and Transformation/01 Selecting and Renaming Columns.py
@@ -28,7 +28,75 @@ display(countries)
 
 # COMMAND ----------
 
+#case sensitvity doesnt matter
+
 countries.select('name','capital','population').display()
+
+# COMMAND ----------
+
+#Better to use when we want to add methods with columns
+#case sensitvity doesnt matter
+
+countries.select(countries['name'],countries['capital'],countries['population']).display()
+
+# COMMAND ----------
+
+#This method is case sensitive
+
+countries.select(countries.NAME , countries.CAPITAL , countries.POPULATION).display()
+
+# COMMAND ----------
+
+from pyspark.sql.functions import col
+
+countries.select(col('name'),col('capital'),col('population')).display()
+
+# COMMAND ----------
+
+#All the methods for selecting columns
+
+countries.select('name','capital','population').display()
+countries.select(countries['name'],countries['capital'],countries['population']).display()
+countries.select(countries.NAME , countries.CAPITAL , countries.POPULATION).display()
+
+from pyspark.sql.functions import col
+countries.select(col('name'),col('capital'),col('population')).display()
+
+
+
+# COMMAND ----------
+
+# alias method
+
+countries.select(countries['name'].alias('Country_Name'),countries['capital'].alias('Capital_city'),countries['population'].alias('population')).display()
+
+# COMMAND ----------
+
+# withColumnRenamed Method
+
+countries.select('name','capital','population').withColumnRenamed('name','Country_Name').withColumnRenamed('capital','Capital_city').display()
+
+# COMMAND ----------
+
+from pyspark.sql.types import StructType, StructField , IntegerType, StringType
+countries_regions_path = '/FileStore/tables/country_regions.csv'
+countries_regions_schema = StructType(
+    [StructField('ID', IntegerType(), False),
+    StructField('NAME', StringType(), False)]
+    )
+
+countries_regions_df = spark.read.csv(path = countries_regions_path, schema=countries_regions_schema, header=True)
+
+# COMMAND ----------
+
+display(countries_regions_df)
+
+# COMMAND ----------
+
+countries_regions_df.select('ID','NAME').withColumnRenamed('NAME', 'Continent').display()
+
+from pyspark.sql.functions import col
+countries_regions_df.select(col('ID'),col('NAME').alias('Continent')).display()
 
 # COMMAND ----------
 
